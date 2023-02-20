d8d9dd6a2fd5212198431bd4317e9b58440ac037
Marius Aiordachioaei <aiordachioaei.marius@gmail.com>
2023-02-20 17:05:01 +0200

    Improve smoke tests

---
+++ src/test/smoke/management.test.ts
@@ -0,0 +1,31 @@
+import { run, runClimat } from '../utils';
+
+describe('smoke', () => {
+  describe('management', () => {
+    it('installs', () => {
+      runClimat('install src/test/smoke/test-toolchain.cli');
+      expect(() => run('test-toolchain')).not.toThrowError();
+
+      expect(run('test-toolchain child --arg1 abc --arg2 bcd')).toEqual(
+        'Hello! I was executed with arg1 = abc and arg2 = bcd',
+      );
+
+      expect(
+        run('test-toolchain child --arg1 value1 --arg2 value2 grandchild1'),
+      ).toEqual('Hello from the other side value1 value2');
+
+      expect(
+        run('test-toolchain child --arg1 val1 --arg2 val2 grandchild2 val3'),
+      ).toEqual('Hello from JS! val3 val1 val2');
+    });
+
+    it('list', () => {
+      expect(runClimat('list')).toContain('test-toolchain');
+    });
+
+    it('uninstalls', () => {
+      runClimat('uninstall test-toolchain');
+      expect(() => run('test-toolchain')).toThrowError();
+    });
+  });
+});
