364cf4f20bcb5990bc3eb51350ac005c11735998
Marius Aiordachioaei <aiordachioaei.marius@gmail.com>
2024-11-24 20:02:22 +0200

    Upgrade docusaurus
    Replace local search engine

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 const CLIMAT = 'CLiMAT';
 
@@ -92,7 +92,7 @@ const config = {
       },
     }),
 
-    plugins: [require.resolve("@cmfcmf/docusaurus-search-local")]
+    plugins: [require.resolve('docusaurus-lunr-search')]
 };
 
 module.exports = config;
--- docs/src/theme/prism-include-languages.js
+++ docs/src/theme/prism-include-languages.js
@@ -53,6 +53,10 @@ export default function prismIncludeLanguages(PrismObject) {
       pattern: /\@alias/,
       alias: 'keyword'
     },
+    'javascript': {
+      pattern: /javascript/,
+      alias: 'keyword'
+    },
     'comment': {
       pattern: /\/\/.*|\/\*[\s\S]*?(?:\*\/|$)/,
       greedy: true
